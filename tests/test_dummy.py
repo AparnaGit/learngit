e3c2e63464f484faeb052c52c3044e90323015dc
aparna aravind <aparna24aravind@gmail.com>
2022-06-16 20:02:03 +0530

    Vanilla unittest

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,22 +1,22 @@
 from pyspark.sql import SparkSession
-from pyspark.testing.utils import  ReusedPySparkTestCase
+import unittest
 
 
-class DummySparkTest(ReusedPySparkTestCase):
-    def setUp(self):
-        self.spark = SparkSession(self.sc)
+class test_pyu(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.spark = (SparkSession
+                     .builder
+                     .master("local[2]")
+                     .appName("dummy")
+                     .getOrCreate())
 
     def test_samplefunc(self):
         df = self.spark.createDataFrame(data=[[1, 'a'], [2, 'b']],
                                         schema=['c1', 'c2'])
         self.assertEqual(df.count(), 1)
 
-    def tearDown(self):
-        self.spark.stop()
+    @classmethod
+    def tearDownClass(cls):
+        cls.spark.stop()
 
-
-
-if __name__ == "__main__":
-    import unittest
-
-    unittest.main()
